b840f139982dc625e3bb600d74ba91873998598b
Justin Young <justin_dean_young@yahoo.com>
2024-03-19 11:14:35 -0400

    project push

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@
 import "./style.css";
 import { UI } from "@peasy-lib/peasy-ui";
 import { Engine, DisplayMode, TileMap, Vector, Loader } from "excalibur";
-import { ExcaliburAstar, ExcaliburGraph, GraphTileMap, aStarNode, GraphNode } from "@excaliburjs/excalibur-pathfinding";
+import { ExcaliburAStar, ExcaliburGraph, GraphTileMap, aStarNode, GraphNode } from "@excaliburjs/excalibur-pathfinding";
 import { Resources, rlSS } from "./resourcses";
 import { Tree, tiles } from "./tiledata";
 import { player } from "./player";
@@ -188,7 +188,7 @@ let myGraphTileMap: GraphTileMap = {
 myDijkstraGraph.addTileMap(myGraphTileMap);
 
 // create astar instance
-let myGraph = new ExcaliburAstar(tilemap);
+let myGraph = new ExcaliburAStar(tilemap);
 
 // start game, add tilemap, and actor
 await game.start(loader);
